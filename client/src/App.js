caf17f67ccab96545c21ecb1e0a2aaa2a84d9d8a
Koltan Huget <koltanhuget@hotmail.com>
2021-11-19 08:41:51 -0700

    basic front end created with search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
 import "./App.css";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
+import Info from "./components/Info";
+import { useState } from "react";
 
 function App() {
+  const [wasteTypes, setWasteTypes] = useState([
+    {
+      category: "pop can",
+      bin: "blue",
+    },
+    {
+      category: "banana peel",
+      bin: "green",
+    },
+    {
+      category: "aluminum foil",
+      bin: "black",
+    },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState("");
+  // search for something to dispose of
   const search = (query) => {
-    console.log(query.text);
+    // replace with fetch request once back end is hooked up
+    let searchResult = wasteTypes.find((wasteType) => {
+      console.log(wasteType.category);
+      return wasteType.category === query.text;
+    });
+    console.log(searchResult);
+    if (searchResult) {
+      setCurrentCategory(searchResult);
+      console.log(currentCategory);
+    } else {
+      setCurrentCategory("");
+      alert("nothing found");
+    }
   };
   return (
     <div className="container">
       <Header />
       <SearchForm onSearch={search} />
-      <p>testing testing testing</p>
+      {currentCategory && <Info wasteType={currentCategory} />}
     </div>
   );
 }
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -3,9 +3,10 @@ import { useState } from "react";
 const SearchForm = ({ onSearch }) => {
   const [text, setText] = useState("");
   const onSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // prevents page from refreshing
 
     if (!text) {
+      // if search bar is empty when user hits submit button:
       alert("Please enter something into the search bar");
       return;
     }
@@ -16,12 +17,19 @@ const SearchForm = ({ onSearch }) => {
   return (
     <form onSubmit={onSubmit}>
       <input
+        style={{
+          width: "100%",
+          height: "40px",
+          margin: "5px",
+          padding: "3px 7px",
+          fontSize: "17px",
+        }}
         type="search"
         placeholder="search for something"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <input type="submit" />
+      <input type="submit" className="btn btn-block" />
     </form>
   );
 };
