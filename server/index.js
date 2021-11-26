7eabb8d3ad390c7ae2e713707bc0e2aef63edc05
sammyebinne <sammyebinne@gmail.com>
2021-11-26 02:17:57 -0700

    dotenv fix

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/routes.js";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-dotenv.config();
 app.use(cors());
 
 app.use("/", router);
